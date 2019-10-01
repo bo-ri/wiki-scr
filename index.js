f0a57693896493bc3ef8de4d363d11bc6a84e47d
TatsuyaKITABORI <45zombie45@gmail.com>
2019-10-01 18:32:24 +0900

    fix: href属性も取得する

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
-
+ 
 const UNIV_LIST_URL = 'https://ja.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC%E3%81%AE%E5%A4%A7%E5%AD%A6%E4%B8%80%E8%A6%A7';
-const TARGET_ATR = ".mw-parser-output > ul > li";
+const TARGET_ATR = ".mw-parser-output > ul > li > a";
 
 // list all university
 const listAllUniv = async function () {
@@ -12,9 +12,18 @@ const listAllUniv = async function () {
   await page.goto(UNIV_LIST_URL);
   // 大学名一覧が表示されるまで待つ
   await page.waitFor(TARGET_ATR);
+  // 大学名とリンク先のhref属性をまとめる
   const data = await page.evaluate((selector) => {
     const list = Array.from(document.querySelectorAll(selector));
-    return list.map(data => data.textContent);
+    var datas = [];
+    for (let i = 0; i < list.length; i++) {
+      var data = {
+        href: list[i].href,
+        textContent: list[i].textContent
+      };
+      datas.push(data);
+    }
+    return datas;
   }, TARGET_ATR);
   console.log(data);
   await browser.close();
